b39cc5984aaefcdf4740b0158994254a7f4bcb05
Rafael Pereira de Melo <63520209+rafaelpdemelo@users.noreply.github.com>
2021-07-07 03:02:34 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'https://podcast-teste-348xftzwm-rafaelpdemelo.vercel.app',
+  baseURL: process.env.API_URL || 'podcast-gazetacast-ian.vercel.app',
 })
