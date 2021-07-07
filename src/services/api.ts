d900ac0bb70d978263ae0336c3dc6c33373823d5
Rafael Pereira de Melo <63520209+rafaelpdemelo@users.noreply.github.com>
2021-07-07 02:06:07 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'https://podappteste.herokuapp.com/'
+  baseURL: process.env.API_URL || 'https://gitlab.com/api/v4/feature_flags/unleash/27955264'
 })
